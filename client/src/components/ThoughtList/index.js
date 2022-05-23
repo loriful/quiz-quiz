c3a0d1e8613907a407e11b0905d3aeafeea7fd5c
Shawn Willard <shawnmwillard@gmail.com>
2022-05-23 18:53:26 -0500

    Favicon component created

--- client/src/components/ThoughtList/index.js
+++ client/src/components/ThoughtList/index.js
@@ -1,6 +1,5 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
+import React from "react";
+import { Link } from "react-router-dom";
 const ThoughtList = ({ thoughts, title }) => {
   if (!thoughts.length) {
     return <h3>No Thoughts Yet</h3>;
@@ -10,7 +9,7 @@ const ThoughtList = ({ thoughts, title }) => {
     <div>
       <h3>{title}</h3>
       {thoughts &&
-        thoughts.map(thought => (
+        thoughts.map((thought) => (
           <div key={thought._id} className="card mb-3">
             <p className="card-header">
               <Link
@@ -19,15 +18,15 @@ const ThoughtList = ({ thoughts, title }) => {
                 className="text-light"
               >
                 {thought.username}
-              </Link>{' '}
+              </Link>{" "}
               thought on {thought.createdAt}
             </p>
             <div className="card-body">
               <Link to={`/thought/${thought._id}`}>
                 <p>{thought.thoughtText}</p>
                 <p className="mb-0">
-                  Reactions: {thought.reactionCount} || Click to{' '}
-                  {thought.reactionCount ? 'see' : 'start'} the discussion!
+                  Reactions: {thought.reactionCount} || Click to{" "}
+                  {thought.reactionCount ? "see" : "start"} the discussion!
                 </p>
               </Link>
             </div>
