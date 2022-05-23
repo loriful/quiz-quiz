3ba86afac23f5e1391cc69b94738eba078a8ce9b
Shawn Willard <shawnmwillard@gmail.com>
2022-05-23 16:40:57 -0500

    homepage icon added

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ThoughtList from '../components/ThoughtList';
+import React from "react";
+import ThoughtList from "../components/ThoughtList";
 
-import { useQuery } from '@apollo/client';
-import { QUERY_THOUGHTS } from '../utils/queries';
+import { useQuery } from "@apollo/client";
+import { QUERY_THOUGHTS } from "../utils/queries";
 
 const Home = () => {
   const { loading, data } = useQuery(QUERY_THOUGHTS);
@@ -11,16 +11,19 @@ const Home = () => {
 
   return (
     <main>
-  <div className="flex-row justify-space-between">
-    <div className="col-12 mb-3">
-      {loading ? (
-        <div>Loading...</div>
-      ) : (
-        <ThoughtList thoughts={thoughts} title="Available Quizzes" />
-      )}
-    </div>
-  </div>
-</main>
+      <div className="flex-row justify-space-between">
+        <div className="col-12 mb-3">
+          {loading ? (
+            <div>Loading...</div>
+          ) : (
+            <div>
+              <ThoughtList thoughts={thoughts} title="Available Quizzes" />
+              <img src="https://blog.hubspot.com/hubfs/google-quiz.jpg"></img>
+            </div>
+          )}
+        </div>
+      </div>
+    </main>
   );
 };
 
